d6e4a4d8e556b8edcc0a08a2756341d4ea0f7415
Tulio <tulio.mminini@gmail.com>
2022-11-02 01:17:20 -0300

    :wrench: trying to fix backend

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(function (req, res, next) {
-//   req.header("Access-Control-Allow-Origin", "https://wheretogo-tm.netlify.app");
-//   req.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   req.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-//   next();
-// });
+app.use(function (req, res, next) {
+  req.header("Access-Control-Allow-Origin", "https://wheretogo-tm.netlify.app");
+  req.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  req.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
+});
 
-app.use(cors({ origin: process.env.REACT_APP_URL }));
+// app.use(cors({ origin: process.env.REACT_APP_URL }));
 
 const uploadImgRouter = require("./routes/uploadimg.routes");
 app.use("/", uploadImgRouter);
